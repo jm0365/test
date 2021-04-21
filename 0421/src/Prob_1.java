225b39fc2d3f57a8274188e5f63c235ae492b320
JMLEE <63764148+jm0365@users.noreply.github.com>
2021-04-21 10:54:52 +0900

    문제 생성

---
+++ 0421/src/Prob_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Prob_1 {
+
+		public static void main(String[] args) {
+
+			Scanner scan = new Scanner(System.in);
+
+
+			
+			
+			
+			
+			
+			
+			}
+			System.out.println(a + "부터 " + b + "까지의 합은 " + sum);
+		}
+
+	}
---
+++ 0421/src/Prob_3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Prob_3 {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+}
+class Grade{
+	int math;
+	int science;
+	int english;
+	public int average() {
+		
+		
+	}
+}
